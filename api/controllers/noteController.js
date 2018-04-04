04ffb30392296fdda60592b85c5c4f969a080c8e
Ryan Silowash <resilowash@gmail.com>
2018-04-04 17:45:22 -0400

    working commit got restify working for post requests

--- api/controllers/noteController.js
+++ api/controllers/noteController.js
@@ -4,6 +4,7 @@ const EOL = require('os').EOL;
 const restify = require('restify');
 const config = require('../config/config.json');
 const fs = require('fs');
+const httpStatus = require('http-status-codes');
 
 console.log("Current Date Time: ", moment().format('MMMM Do YYYY, h:mm:ss a'));
 
@@ -18,9 +19,21 @@ const createNote = (title, text, date) => {
 }
 
 let server = restify.createServer();
+server.use(restify.plugins.bodyParser());
 
-server.post('/note', () => {
 
+server.post('/note', (req, res, next) => {
+  console.log('Note Post');
+  //console.log('request: ', req);
+    //let jsonBody = JSON.parse(req.body);
+  //console.log("Title: ", jsonBody.Title );
+  console.log('REquest body: ', req.body);
+  console.log('Request params: ', req.params);
+  res.send(httpStatus.OK);
+  return next();
 });
 
-console.log("Your Note: %s Title - %s %s Text - %s %s Date - %s", EOL, myNote.title, EOL, myNote.text, EOL, myNote.date );
+server.listen(config.noteController.port, ()=>{
+  console.log('Ready on %s', server.url);
+});
+//console.log("Your Note: %s Title - %s %s Text - %s %s Date - %s", EOL, myNote.title, EOL, myNote.text, EOL, myNote.date );
