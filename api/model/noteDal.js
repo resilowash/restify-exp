59fd21f4daef5a01b029f92b78f976e8b0b01f86
Ryan Silowash <resilowash@gmail.com>
2018-04-04 21:59:20 -0400

    working on database connectivity

--- api/model/noteDal.js
+++ api/model/noteDal.js
@@ -14,23 +14,13 @@ class NoteDal {
         });
     }
 
-    async connect() {
-        try {
-            console.log("this client: ", this.client);
-            await this.client.connect();
-        }
-        catch(e) {
-            console.log("Error Connecting:  %s %s", this.client, e);
-        }
-    }
-
     async createNewNote(note) {
         let querytext = ('INSERT INTO note(guid, subject, notebody, datecreated)VALUES($1, $2, $3, $4)');
         const values = [note.guid, note.title, note.text, note.date];
 
         try{
-          let client =  this.connect();
-          const resp =  await client.query(querytext, values);
+          this.client.connect();
+          const resp =  await this.client.query(querytext, values);
           return resp;
         }
         catch(e){
