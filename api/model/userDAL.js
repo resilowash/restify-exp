751f92cbf4b8babc7f814c8cd001b4ac3247fc41
Ryan Silowash <rsilowash@iq-inc.com>
2018-06-20 15:37:47 -0400

    added an update user method

--- api/model/userDAL.js
+++ api/model/userDAL.js
@@ -16,8 +16,26 @@ class UserDAL extends BaseDAL {
       return resp;
     }
     catch(e){
-      console.log('Failed in db', e);
+      console.log('Failed to create new user', e);
     }
   }
 
+  async updateUser(userModel) {
+    let querytext = ('Update note_user SET email = $1, password = $2 WHERE guid = $3');
+    const values = [userModel.email, userModel.password, userModel.guid];
+
+    try{
+      await this.client.connect();
+      const resp =  await this.client.query(querytext, values);
+      await this.client.end();
+      return resp;
+    }
+    catch(e){
+      console.log('Failed to Update UserModel', e);
+    }
+  }
+
+  async getUserDetails(id) {
+    let querytext = ('SELECT * FROM note_user'); 
+  }
 }
